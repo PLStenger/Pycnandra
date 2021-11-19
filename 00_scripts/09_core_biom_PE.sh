cffc3cda59b4cc2665795d73dd32c9e030f99384
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2021-11-19 11:48:25 +1100

    Update 09_core_biom_PE.sh

--- 00_scripts/09_core_biom_PE.sh
+++ 00_scripts/09_core_biom_PE.sh
@@ -30,7 +30,7 @@ qiime feature-table filter-samples \
         --i-table core/RarTable.qza \
         --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
         --p-where "[#SampleID] IN ('Pa-1-10', 'Pa-1-5', 'Pa-2-10', 'Pa-2-5', 'Pa-3-10', 'Pa-3-5', 'Pa-4-10', 'Pa-4-5', 'Pa-5-10', 'Pa-5-5', 'Pa-6-10', 'Pa-6-5', 'Pf-1-10', 'Pf-1-5', 'Pf-2-10', 'Pf-2-5', 'Pf-3-10', 'Pf-3-5', 'Pf-4-10', 'Pf-4-5', 'Pf-5-10', 'Pf-5-5', 'Pf-6-10', 'Pf-6-5')"  \
-        --o-filtered-table subtables/RarTable-all.qza
+        --o-filtered-table core/RarTable-all.qza
 
         
 # Aim: Identify "core" features, which are features observed,
@@ -38,13 +38,13 @@ qiime feature-table filter-samples \
 
         
 qiime feature-table core-features \
-        --i-table subtables/RarTable-all.qza \
+        --i-table core/RarTable-all.qza \
         --p-min-fraction 0.1 \
         --p-max-fraction 1.0 \
         --p-steps 10 \
         --o-visualization visual/CoreBiom-all.qzv  
 
-qiime tools export --input-path subtables/RarTable-all.qza --output-path export/subtables/RarTable-all    
+qiime tools export --input-path core/RarTable-all.qza --output-path export/core/RarTable-all    
         
 #qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
 qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/visual/CoreBiom-all
@@ -55,11 +55,11 @@ qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/vis
 
 # Aim: Convert to/from the BIOM table format
 
-biom convert -i export/subtables/RarTable-all/feature-table.biom -o export/subtables/RarTable-all/table-from-biom.tsv --to-tsv
+biom convert -i export/core/RarTable-all/feature-table.biom -o export/core/RarTable-all/table-from-biom.tsv --to-tsv
 
  # Aim: Remove first line and rename '#OTU ID' into 'ASV'
 
- sed '1d ; s/\#OTU ID/ASV_ID/' export/subtables/RarTable-all/table-from-biom.tsv > export/subtables/RarTable-all/ASV.tsv
+ sed '1d ; s/\#OTU ID/ASV_ID/' export/core/RarTable-all/table-from-biom.tsv > export/core/RarTable-all/ASV.tsv
 
 
 ###############################################################
@@ -77,20 +77,20 @@ conda activate qiime2-2021.4
         --i-table core/RarTable.qza \
         --m-metadata-file $METADATA_ITS/sample-metadata.tsv \
         --p-where "[#SampleID] IN ('Pa-1-10', 'Pa-1-5', 'Pa-2-10', 'Pa-2-5', 'Pa-3-10', 'Pa-3-5', 'Pa-4-10', 'Pa-4-5', 'Pa-5-10', 'Pa-5-5', 'Pa-6-10', 'Pa-6-5', 'Pf-1-10', 'Pf-1-5', 'Pf-2-10', 'Pf-2-5', 'Pf-3-10', 'Pf-3-5', 'Pf-4-10', 'Pf-4-5', 'Pf-5-10', 'Pf-5-5', 'Pf-6-10', 'Pf-6-5')"  \
-        --o-filtered-table subtables/RarTable-all.qza
+        --o-filtered-table core/RarTable-all.qza
            
            
 # Aim: Identify "core" features, which are features observed,
      # in a user-defined fraction of the samples
 
 qiime feature-table core-features \
-        --i-table subtables/RarTable-all.qza \
+        --i-table core/RarTable-all.qza \
         --p-min-fraction 0.1 \
         --p-max-fraction 1.0 \
         --p-steps 10 \
         --o-visualization visual/CoreBiom-all.qzv
         
-qiime tools export --input-path subtables/RarTable-all.qza --output-path export/subtables/RarTable-all    
+qiime tools export --input-path core/RarTable-all.qza --output-path export/core/RarTable-all    
         
 #qiime tools export --input-path visual/CoreBiomAll.qzv --output-path export/visual/CoreBiomAll
 qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/visual/CoreBiom-all
@@ -100,7 +100,7 @@ qiime tools export --input-path visual/CoreBiom-all.qzv --output-path export/vis
 
 # Aim: Convert to/from the BIOM table format
 
-biom convert -i export/subtables/RarTable-all/feature-table.biom -o export/subtables/RarTable-all/table-from-biom.tsv --to-tsv
+biom convert -i export/core/RarTable-all/feature-table.biom -o export/core/RarTable-all/table-from-biom.tsv --to-tsv
 
  # Aim: Remove first line and rename '#OTU ID' into 'ASV'
  
